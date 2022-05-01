e57187e6c769650399fabcda24b5360b242f6469
Wout Vandesompele <wowtvds@gmail.com>
2022-05-01 13:51:28 +0200

    feat: add capacitor for android

---
+++ frontend/android/app/src/main/java/xyz/kojo/MainActivity.java
@@ -0,0 +1,5 @@
+package xyz.kojo;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'xyz.kojo',
+  appName: 'kojo',
+  webDir: 'out',
+  bundledWebRuntime: false
+};
+
+export default config;
