6d7badbc736e62c6019a0e7f8a6e53a8c8e240a3
Wout Vandesompele <wowtvds@gmail.com>
2022-05-12 00:14:15 +0200

    feat: add android project

--- frontend/app/capacitor.config.ts
+++ frontend/app/capacitor.config.ts
@@ -3,11 +3,10 @@ import { CapacitorConfig } from "@capacitor/cli";
 const config: CapacitorConfig = {
 	appId: "xyz.kojo",
 	appName: "kojo",
-	webDir: "out",
+	webDir: "build",
 	bundledWebRuntime: false,
 	plugins: {
 		SplashScreen: {
-			launchAutoHide: false,
 			androidScaleType: "CENTER_CROP",
 		},
 	},
